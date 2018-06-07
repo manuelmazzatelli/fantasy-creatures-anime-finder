c735e62060363ce19e5e1b1334f8aad3d4034a56
manuel mazzatelli <manuel.narut@gmail.com>
2018-06-07 13:01:45 +0200

    test anime call

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,7 @@ function datiArrivati(data) {
     console.log(data)
     let container = jQuery("#creatures-container")
     for (let index = 0; index < data.length; index++) {
-            jQuery(
+            var blogCard = jQuery(
                 [
                     '<div class="blog-card '+( index%2 ? '' : 'alt')+'">',
                     '<div class="photo photo" style="background-image: url(\''+data[index].url_img+'\')"></div>',
@@ -13,11 +13,18 @@ function datiArrivati(data) {
                     '<div class="description">',
                     '<h1>'+data[index].nome+'</h1>',
                     '<p class="summary">'+data[index].descrizione+'</p>',
-                    '<a href="#">Read More</a>',
+                    '<a href="#" data-search="'+data[index].nome+'">Read More</a>',
                     '</div>',
                     '</div>'
                 ].join("\n")
             ).appendTo(container);
+
+            blogCard.find("a").click(function(event) {
+                $.getJSON("https://api.jikan.moe/search/anime/"+$(this).data("search")+"/", function(data) {
+                    console.log(data.result.slice(0,10));
+                });
+                event.preventDefault();
+            });
     }
 }
 
